d5ac5f0837130b8f1622f3473626eb45eeb39eeb
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-02-23 10:11:49 +0300

    a bunch of stuff i forgot to add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :devises, class_name: "Users::Devise"
+  devise_for :users
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
